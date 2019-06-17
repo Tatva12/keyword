7e11687a325f9a09455faee0cb33d6d61eed41ec
Tatva <tatvakeswani@gmail.com>
2019-06-17 16:28:47 +0530

    Added Swap Keyword Feature. Still needs to be improved. (contains a problem)

--- Keyword 1.1.3.py
+++ Keyword 1.1.3.py
@@ -6,7 +6,7 @@ currentfoe = "Darshan"
 currentkeyword = "Rohit"
 menuactions = ["use item", "swap keyword", "fight", "run"]
 bag = ["Potion 50 coins", "Super Potion - 100 coins", "Hyper Potion - 500 coins"]
-mykeywords = ["Tatva", "Varun", "Rushil", "Rithik", "Darshan"]
+mykeywords = ["Tatva", "Varun", "Rushil", "Rithik", "Darshan", "Rohit"]
 run = [99, 34, 56, 83, 12, 25, 8, 6]
 foehp = 100
 myhp = 100
@@ -23,6 +23,8 @@ pjd = 0
 bangd = 0
 x = 0
 steps = 0
+dmgdealer = currentkeyword
+dmgreceiver = currentfoe
 totalcoins = 1000
 lostcoins = random.randrange(45, 90, 5)
 gainedcoins = random.randrange(45, 100, 5)
@@ -31,50 +33,79 @@ gainedcoins = random.randrange(45, 100, 5)
 
 
 def rap():
-    global rapd, currentkeyword, foehp, myhitmultiplier, foehitmultiplier
-    rapd = random.randint(11, 25) * myhitmultiplier * foedefmultiplier
-    print(currentkeyword + " used Rap")
+    global rapd, currentkeyword, foehp, myhp, myhitmultiplier, foehitmultiplier, mydefmultiplier, foedefmultiplier, currentfoe
+    if dmgreceiver == currentfoe:
+        rapd = random.randint(22, 35) * myhitmultiplier * foedefmultiplier
+        foehp -= int(rapd)
+    elif dmgreceiver == currentkeyword:
+        rapd = random.randint(22, 35) * foehitmultiplier * mydefmultiplier
+        myhp -= int(rapd)
+    print(dmgdealer + " used Rap")
     print("It did " + str(rapd) + " damage")
-    foehp -= int(rapd)
 
 
 def gk():
-    global gkd, currentkeyword, foehp, myhitmultiplier, foedeftmultiplier
-    gkd = random.randint(10, 20) * myhitmultiplier * foedefmultiplier
-    print(currentkeyword + " used GK")
+    global gkd, currentkeyword, currentfoe, foehp, myhp, myhitmultiplier, foedeftmultiplier, foehitmultiplier, mydefmultiplier
+    if dmgreceiver == currentfoe:
+        gkd = random.randint(15, 25) * myhitmultiplier * foedefmultiplier
+        foehp -= gkd
+    elif dmgreceiver == currentkeyword:
+        gkd = random.randint(15, 25) * mydefmultiplier * foehitmultiplier
+        myhp -= gkd
+    print(dmgdealer + " used GK")
     print("It did " + str(gkd) + " damage")
-    foehp -= gkd
 
 
 def bite():
-    global bited, currentkeyword, myhp, foehitmultiplier, mydefmultiplier
-    bited = random.randint(20, 25) * foehitmultiplier * mydefmultiplier
-    print(currentfoe + " used Bite")
+    global bited, currentkeyword, currentfoe, myhp, foehp, foehitmultiplier, mydefmultiplier, myhitmultiplier, foedefmultiplier
+    if dmgreceiver == currentfoe:
+        bited = random.randint(20, 25) * foehitmultiplier * mydefmultiplier
+        myhp -= bited
+    elif dmgreceiver == currentkeyword:
+        bited = random.randint(20, 25) * myhitmultiplier * foedefmultiplier
+        foehp -= bited
+    print(dmgdealer + " used Bite")
     print("It did " + str(bited) + " damage")
-    myhp -= bited
 
 
-def football():
-    global currentkeyword, currentfoe, footballd, myhp, foedefmultiplier, mydefmultiplier
+def football():  # GO THROUGH MIGHT BE SOME LOGICAL ERROR
+    global currentkeyword, currentfoe, footballd, myhp, foehp, foehitmultiplier, foedefmultiplier, mydefmultiplier, myhitmultiplier
     footballd = random.randint(30, 40)
     chance = [1]
-    print(currentfoe + " used Football!")
-    print(currentkeyword + " started playibg football with " + currentfoe)
-    if x in chance:
-        print(currentkeyword + " fell down and hurt himself ")
-        print(currentkeyword + " lost " + str(footballd) + " HP!")
-        myhp -= footballd
-    else:
-        print("Both of them got tired...")
-        print(currentfoe + "'s and " + currentkeyword + "'s defense fell")
-        if foedefmultiplier <= 0.5:
-            print(currentfoe + "'s defense is already low")
+    print(dmgdealer + " used Football!")
+    print(dmgreceiver + " started playibg football with " + dmgdealer)
+    if dmgreceiver == currentkeyword:
+        if x in chance:
+            print(currentkeyword + " fell down and hurt himself ")
+            print(currentkeyword + " lost " + str(footballd) + " HP!")
+            myhp -= footballd
         else:
-            foedefmultiplier -= 0.1
-        if mydefmultiplier <= 0.5:
-            print(currentkeyword + "'s defense is already low")
+            print("Both of them got tired...")
+            print(currentfoe + "'s and " + currentkeyword + "'s defense fell")
+            if foedefmultiplier <= 0.5:
+                print(currentfoe + "'s defense is already low")
+            else:
+                foedefmultiplier -= 0.1
+            if mydefmultiplier <= 0.5:
+                print(currentkeyword + "'s defense is already low")
+            else:
+                mydefmultiplier -= 0.1
+    elif dmgreceiver == currentfoe:
+        if x in chance:
+            print(dmgreceiver + " fell down and hurt himself ")
+            print(dmgreceiver + " lost " + str(footballd) + " HP!")
+            foehp -= footballd
         else:
-            mydefmultiplier -= 0.1
+            print("Both of them got tired...")
+            print(dmgdealer + "'s and " + dmgreceiver + "'s defense fell")
+            if foedefmultiplier <= 0.5:
+                print(dmgreceiver + "'s defense is already low")
+            else:
+                foedefmultiplier -= 0.1
+            if foedefmultiplier <= 0.5:
+                print(dmgreceiver + "'s defense is already low")
+            else:
+                foedefmultiplier -= 0.1
 
 
 def stare():
@@ -89,51 +120,70 @@ def stare():
 
 
 def pj():
-    global pjd, currentkeyword, myhp, bangd
-    print(currentfoe + " used PJ.")
-    print(currentkeyword + " hates his life.")
-    print(currentkeyword + " feels like banging his head on the wall!")
-    print(currentkeyword + " learned Bang")
-    print(currentkeyword + " Banged his head on the wall")
-    print(currentkeyword + " forgot Bang")
+    global pjd, myhp, foehp, bangd, mycurrentmoveset, dmgdealer, dmgreceiver, currentkeyword, currentkeyword
+    print(dmgdealer + " used PJ.")
+    print(dmgreceiver + " hates his life.")
+    print(dmgreceiver + " feels like banging his head on the wall!")
+    print(dmgreceiver + " learned Bang")
+    print(dmgreceiver + " Banged his head on the wall")
+    print(dmgreceiver + " forgot Bang")
     bangd = random.randint(50, 75)
-    myhp -= bangd
+    if dmgreceiver == currentfoe:
+        foehp -= bangd
+        print(dmgreceiver + " lost " + str(bangd) + " HP")
+    elif dmgreceiver == currentkeyword:
+        myhp -= bangd
+        print(dmgreceiver + " lost " + str(bangd) + " HP")
 
 
 def lipstick():
-    global currentfoe, currentkeyword, foehitmultiplier
-    print(currentkeyword + " used Lipstick")
-    print(currentfoe + " fell in love with " + currentkeyword)
-    if foehitmultiplier <= 0.5:
-        print(currentfoe + "'s hitmultiplier is already low")
-    else:
-        print(currentfoe + "'s hitmultiplier fell harshly")
-        foehitmultiplier -= 0.3
+    global currentfoe, currentkeyword, foehitmultiplier, myhitmultiplier, dmgreceiver, dmgdealer
+    print(dmgdealer + " used Lipstick")
+    print(dmgreceiver + " fell in love with " + dmgdealer)
+    if dmgreceiver == currentfoe:
+        if foehitmultiplier <= 0.5:
+            print(dmgdealer + "'s hitmultiplier is already low")
+        elif foehitmultiplier > 0.5:
+            print(currentfoe + "'s hitmultiplier fell harshly")
+            foehitmultiplier -= 0.3
+    elif dmgreceiver == currentkeyword:
+        if myhitmultiplier <= 0.5:
+            print(dmgreceiver + "'s hitmultiplier is already low")
+        else:
+            print(dmgreceiver + "'s hitmultiplier fell harshly")
+            myhitmultiplier -= 0.3
 
 
 def pokemon():
-    global currentkeyword, currentfoe, foehitmultiplier, foehp, x
-    print(currentkeyword + " used Pokemon")
-    print(currentfoe + " started playing Pokemon with " + currentkeyword)
-    print(currentfoe + " died in a gym battle in Pokemon.")
-    print(currentfoe + " hates his life.")
-    print(currentfoe + " feels like jumping off the roof.")
+    global currentkeyword, currentfoe, foehitmultiplier, myhitmultiplier, foehp, myhp, x
+    print(dmgdealer + " used Pokemon")
+    print(dmgreceiver + " started playing Pokemon with " + dmgdealer)
+    print(dmgreceiver + " died in a gym battle in Pokemon.")
+    print(dmgreceiver + " hates his life.")
+    print(dmgreceiver + " feels like jumping off the roof.")
 
     x = random.randint(1, 10)
     chance = [2, 4]
 
     if x in chance:
-        print(currentfoe + " jumped off the roof. He couldn't take it anymore...")
-        foehp = 0
+        print(dmgreceiver + " jumped off the roof. He couldn't take it anymore...")
+        if dmgreceiver == currentfoe:
+            foehp = 0
+        elif dmgreceiver == currentkeyword:
+            myhp = 0
     else:
-        print(currentfoe + " decided not today! I'll kill this retard's ***!")
-        print(currentfoe + "'s hitmultiplier rose harshly!")
-        foehitmultiplier += 0.3
+        print(dmgreceiver + " decided not today! I'll kill this retard's ***!")
+        print(dmgreceiver + "'s hitmultiplier rose harshly!")
+        if dmgreceiver == currentfoe:
+            foehitmultiplier += 0.3
+        elif dmgreceiver == currentkeyword:
+            myhitmultiplier += 0.3
 
 
-def darshanmove():
-    global randmove
-    randmove = random.choice(darshanmoveset)
+def foemove():
+    global randmove, foecurrentmoveset, currentfoe
+    randmove = random.choice(foecurrentmoveset)
+    dmgdealer = currentfoe
     if randmove == "Bite":
         bite()
     elif randmove == "Football":
@@ -147,14 +197,16 @@ def darshanmove():
 # Movesets
 rohitmoveset = ["Rap", "GK", "Lipstick", "Pokemon"]
 darshanmoveset = ["Bite", "Football", "Stare", "PJ"]
-tatvamoveset = []
+tatvamoveset = ["Smile"]
 varunmoveset = []
 rithikmoveset = []
 rushilmoveset = []
 
+
 print("A wild " + currentfoe + " appeared")
 print("You sent " + currentkeyword + "!")
-
+mycurrentmoveset = rohitmoveset
+foecurrentmoveset = darshanmoveset
 
 while myhp > 0 and foehp > 0:
     steps += 1
@@ -162,7 +214,7 @@ while myhp > 0 and foehp > 0:
     print(currentkeyword + "'s HP = " + str(myhp))
 
     action1 = input("What would you like to do?\n")
-    if action1 == menuactions[0]:  # Use Item
+    if action1 == menuactions[0]:    # Use Item
         print("Items in bag")
         print(*bag, sep="\n")
         action2 = input("What do you want to use?\n")
@@ -174,7 +226,7 @@ while myhp > 0 and foehp > 0:
                 print(currentkeyword + " gained 20 HP")
                 print(currentkeyword + " has " + str(myhp) + "HP now")
                 print("You have " + str(tatalcoins) + " now.")
-                darshanmove()
+                foemove()
             else:
                 print("Not enough coins!")
             continue
@@ -185,7 +237,7 @@ while myhp > 0 and foehp > 0:
                 print(currentkeyword + " gained 50 HP")
                 print(currentkeyword + " has " + str(myhp) + "HP now")
                 print("You have " + str(tatalcoins) + " now.")
-                darshanmove()
+                foemove()
             else:
                 print("Not enough coins")
             continue
@@ -203,58 +255,72 @@ while myhp > 0 and foehp > 0:
             print("invalid action")
             continue
         break
-    elif action1 == menuactions[1]:
+    elif action1 == menuactions[1]:  # Swap Keyword
         print(*mykeywords, sep="\n")
-        action2 = input("Which keyword do you choose?\n")  # Swap Keyword
+        action2 = input("Which keyword do you choose?\n")
         if action2 == currentkeyword:
             print(currentkeyword + " is already in battle!")
             continue
         elif action2 == mykeywords[0]:
             print("You sent out " + mykeywords[0])
             currentkeyword = mykeywords[0]
+            mycurrentmoveset = tatvamoveset
+            dmgdealer = currentkeyword
             continue
         elif action2 == mykeywords[1]:
             print("You sent out " + mykeywords[1])
             currentkeyword = mykeywords[1]
+            mycurrentmoveset = varunmoveset
+            dmgdealer = currentkeyword
             continue
         elif action2 == mykeywords[2]:
             print("You sent out " + mykeywords[2])
+            mycurrentmoveset = rushilmoveset
             currentkeyword = mykeywords[2]
+            dmgdealer = currentkeyword
             continue
         elif action2 == mykeywords[3]:
             print("You sent out " + mykeywords[3])
             currentkeyword = mykeywords[3]
+            mycurrentmoveset = rithikmoveset
+            dmgdealer = currentkeyword
             continue
         elif action2 == mykeywords[4]:
             print("You sent out " + mykeywords[4])
             currentkeyword = mykeywords[4]
+            mycurrentmoveset = darshanmoveset
+            dmgdealer = currentkeyword
             continue
-    elif action1 == menuactions[2]:
-        print(*rohitmoveset, sep="\n")
-        action2 = input("Which move do you want to use?\n")  # Fight
-        if action2 in rohitmoveset and action2 == "Rap":
+    elif action1 == menuactions[2]:  # Fight
+        print(*mycurrentmoveset, sep="\n")
+        action2 = input("Which move do you want to use?\n")
+        if action2 in mycurrentmoveset and action2 == "Rap":
             rap()
-            darshanmove()
+            foemove()
             continue
-        elif action2 in rohitmoveset and action2 == "GK":
+        elif action2 in mycurrentmoveset and action2 == "GK":
             gk()
-            darshanmove()
+            foemove()
             continue
-        elif action2 in rohitmoveset and action2 == "Lipstick":
+        elif action2 in mycurrentmoveset and action2 == "Lipstick":
             lipstick()
-            darshanmove()
+            foemove()
             continue
-        elif action2 in rohitmoveset and action2 == "Pokemon":
+        elif action2 in mycurrentmoveset and action2 == "Pokemon":
             pokemon()
             if foehp == 0:
                 continue
-            else:
-                darshanmove()
-                continue
+        elif action2 in mycurrentmoveset and action2 == "PJ":
+            pj()
+            foemove()
             continue
-    elif action1 == menuactions[3]:
+        else:
+            foemove()
+            continue
+        continue
+    elif action1 == menuactions[3]:  # Run
         x = random.randrange(1, 101)
-        if x in run:  # Run
+        if x in run:
             print("You failed to run away...")
             continue
         else:
