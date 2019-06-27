79fdb296c14bbd44a0483d5e672d9682bf49bdb4
Tatva <tatvakeswani@gmail.com>
2019-06-27 19:57:29 +0530

    Started Rewriitng the whole program in a better way...contains bugs...incomplete

---
+++ Keyword redo/Lists.py
@@ -0,0 +1,6 @@
+keywords = ["Darshan", "Rohit", "Rushil", "Rithik", "Varun", "Tatva"]
+mykeywords = ["Darshan", "Rohit"]
+
+
+menuactions = ["use item", "swap keyword", "fight", "run"]
+bag = ["Potion", "Super Potion", "Hyper Potion"]
---
+++ Keyword redo/keywords.py
@@ -0,0 +1,11 @@
+
+def rohit():
+    global currentkeywordhp, currentmoveset
+    currentkeywordhp = 139
+    currentmoveset = ["Rap", "GK", "Lipstick", "Pokemon"]
+
+
+def darshan():
+    global currentkeywordhp, currentmoveset
+    currentkeywordhp = 178
+    currentmoveset = ["Football", "PJ", "Stare", "Bite"]
