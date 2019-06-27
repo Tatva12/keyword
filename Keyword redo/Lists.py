c64d7d6c7be8e6248f40a1af4a62ff7b43019fde
Tatva <tatvakeswani@gmail.com>
2019-06-27 20:26:07 +0530

    Ignore this commit

--- Keyword redo/Lists.py
+++
@@ -1,6 +0,0 @@
-keywords = ["Darshan", "Rohit", "Rushil", "Rithik", "Varun", "Tatva"]
-mykeywords = ["Darshan", "Rohit"]
-
-
-menuactions = ["use item", "swap keyword", "fight", "run"]
-bag = ["Potion", "Super Potion", "Hyper Potion"]
